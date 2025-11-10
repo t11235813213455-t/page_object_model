b769ae4f49239bb599fcc4ac22c7424e4b910fd3
t11235813213455-t <t11235813213455@gmail.com>
2025-11-10 12:43:15 +0300

    add should_be_login_page() method

--- Pages/main_page.py
+++ Pages/main_page.py
@@ -1,7 +1,7 @@
 from .base_page import BasePage
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from .locators import MainPageLocators
+from Pages.locators import MainPageLocators
 
 class MainPage(BasePage):
 
--- test_main_page.py
+++ test_main_page.py
@@ -1,12 +1,15 @@
 from Pages.main_page import MainPage
 from Pages.base_page import BasePage
+from Pages.login_page import LoginPage
 import time
 from selenium.common.exceptions import NoSuchElementException
+
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)    
     page.open()   
     page.should_be_login_link()
-    time.sleep(1)
-    #page.go_to_login_page() 
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
